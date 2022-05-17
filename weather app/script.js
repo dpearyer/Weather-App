55c2dbf84fe85d82fd840920da12a9a74c2cf4d5
Dominique P <dpear1050@gmail.com>
2022-05-17 14:21:47 -0400

    Update script.js

--- weather app/script.js
+++ weather app/script.js
@@ -1,3 +1,47 @@
 let weather = {
-    "apiKey": "ba7ce67280c957a39ee525b7269ae805"
-}
+    "apiKey": "ba7ce67280c957a39ee525b7269ae805",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + 
+        "&units=metric&appid=" + this.apiKey
+        )
+        .then((response) => {
+            if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found.");
+            }
+            return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img.wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity " + humidity +
+        "%";
+        document.querySelector(".wind").innerText = "Wind Speed " + speed + "Miles/Hr";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+
+};
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if(event.key == "Enter") {
+    weather.search();
+    }
+});
+
+weather.fetchWeather("Atlanta");
